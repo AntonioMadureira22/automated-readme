02c507455adcf93d39ee435e7a954de7927c2599
Antonio Madureira <antoniomadureira2222@gmail.com>
2021-10-14 22:35:41 -0400

    added more questions to the index file

--- Develop/index.js
+++ Develop/index.js
@@ -1,66 +1,77 @@
 // TODO: Include packages needed for this application
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require('inquirer')
+// const generateMarkdown = require("./utils/generateMarkdown");
 const fs = require("fs");
-const utils = require("utils");
-const emailVaildator = require("email-validator");
+// const emailVaildator = require("email-validator");
 
 
 // prompt user for start of readme generator
 //const promptUser = readmeData => {
-    //console.log(
-      //  "Welcome to the ReadME Generator! Let's begin with a few questions"
-    //);
+//console.log(
+//  "Welcome to the ReadME Generator! Let's begin with a few questions"
+//);
 //}
 //if (!readmeData) {
-    //readmeData = [];
+//readmeData = [];
 //}
 // TODO: Create an array of questions for user input
-const promptUser = () => {
+// const promptUser = () => {
 
- return inquirer.prompt([{
-     type: "input",
-     name: "Title",
-     message: "What is the title of your project? (required)",
-     validate: titleInput => {
-         if (!titleInput) {
-             return true;
-         } else{
-             console.log("A title name is required, please enter project name!");
-             return false;
-         } else{
-             console.log("A title name is required, please enter project name!");
-             return true;
-         }
+function runPromps() {
 
-         
-        },
+inquirer
+    .prompt([{
+        type: "input",
+        name: "title",
+        message: "What is the title of your project? (required)",
+    },
+    {
         type: "input",
         name: "desc",
         message: "Add a description for the application (required)",
-        validate: descInput =>{
-            if (!descInput) {
-                console.log("You need to add a description for your project!");
-                return false;
-            }
-            return true;
-
-        },
-
         
+    },
+    {
         type: "input",
         name: "use",
         message: ("Provide instructions on how to use the app"),
-        
+    },
+    {
+        type: "input",
+        name: "contribute",
+        message: "Write your contributions",
+    },
+    {
+        type: "input",
+        name: "username",
+        message: ("What is your GitHub username?"),
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Select which license's to be used.",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email? (required)",
+    },
+    {
 
-    }])
+    }
+
+    ]).then((data) => {
+        console.log(data)
+    })
 }
 
+runPromps()
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() { }
 
 // Function call to initialize app
-init();
+//init();
